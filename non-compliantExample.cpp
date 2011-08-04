7be9e982d2df69cf0e8c24687906bb015acf32ac
Terrence Chida <chidat91@gmail.com>
2011-08-04 11:56:18 -1000

    added non-compliant example

---
+++ non-compliantExample.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <string.h>
+#include "iostream"
+using namespace std;
+bool isValidLogin(char name[], char pass[])
+{
+    if(strcmp(name, "rinkesh") == 0 && strcmp(pass, "rinkesh") == 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+int main(int argc, char**argv)
+{
+    cout<<"\nBanking Application.\n";
+    cout<<"\nWelcome to the Bank ATM service.";
+    cout<<"\nPlease enter your username and password to enter the  
+    system.\n";
+    cout<<"Your username and password should be maximum 10 characters 
+    long.\n";
+    while(true)
+    {
+          int authentication(0);
+          char cUsername[10], cPassword[10];
+          puts("\nEnter username: ");
+          gets(cUsername);
+          puts("\nEnter password : ");
+          gets(cPassword);
+          
+          if(isValidLogin(cUsername, cPassword))
+          {
+            authentication = 1;
+          }
+          if(authentication)
+          {
+            printf("Access granted \n You can access your account 
+            now.");
+          } 
+          else 
+          {
+            printf("Wrong username or password please enter the username 
+            again.\n");
+          }
+    }
+    return 0;
+}
